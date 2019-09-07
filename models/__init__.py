d612bbfc8d44e5791cd6748c8f2d2a985316ca43
Alwin <alwin.zhang@belloai.com>
2019-09-07 22:43:40 +0800

    利用gensim word2vec 实现的deepwalk micro-f1: 0.6777546777546778 macro-f1: 0.59089798375305

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+# -*-coding: utf-8 -*-
+"""
+    @project:PycharmProjects
+    @author:alwin
+    @file:__init__.py.py
+    @time:2019-09-07 21:50:25
+    @github:alwin114@hotmail.com
+"""
---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+# -*-coding: utf-8 -*-
+"""
+    @project:PycharmProjects
+    @author:alwin
+    @file:__init__.py.py
+    @time:2019-09-07 22:32:17
+    @github:alwin114@hotmail.com
+"""
